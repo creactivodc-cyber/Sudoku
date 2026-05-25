0b0ce066d7450f575560c2304241ba8de4ff72fb
robert-funes <creactivodc@gmail.com>
2026-05-24 22:50:28 -0500

    Agregar método de validación en la clase Grid

--- src/main/java/com/example/sudoku/models/Grid.java
+++ src/main/java/com/example/sudoku/models/Grid.java
@@ -1,9 +1,14 @@
 package com.example.sudoku.models;
 
 
-
+// Clase para generar la cuadricula de números aleatoriamente
 public class Grid {
 
-    for
+    boolean validation () {
+        for (int i = 0; i < 6; i++) {
+
+        }
+        return false;
+    }
 
 }
