e767a4c7edbbac8a4a2c4dacce13a1e129cdbcef
robert-funes <creactivodc@gmail.com>
2026-06-01 22:33:49 -0500

    feat: implementar generación aleatoria de la cuadrícula en la clase Grid

--- src/main/java/com/example/sudoku/controllers/GameController.java
+++ src/main/java/com/example/sudoku/controllers/GameController.java
@@ -3,6 +3,7 @@ package com.example.sudoku.controllers;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import com.example.sudoku.models.Grid;
 
 public class GameController {
 
@@ -12,5 +13,9 @@ public class GameController {
 
     @FXML void onActionHelpButton() {}
     @FXML void onActionEnterButton(){}
+
+    private Grid grid = new Grid();
+    
+    int result = grid.generateGrid();
 
 }
--- src/main/java/com/example/sudoku/models/Grid.java
+++ src/main/java/com/example/sudoku/models/Grid.java
@@ -1,12 +1,15 @@
 package com.example.sudoku.models;
+import java.util.ArrayList;
 
 /** Clase para generar la cuadrícula de números aleatoriamente */
 public class Grid {
     
     /** Tamaño de la cuadrícula */
     int size = 6;
-    /** Cuadrícula de números */
-    int grid[][] = new int[size][size];
+
+    /** Listas dinámicas para almacenar los números de las filas y columnas */
+    ArrayList<Integer> gridRowList = new ArrayList<>();
+    ArrayList<Integer> gridColumnList;
     
     /**
      * Valida si un número puede ser colocado en una posición específica de la cuadrícula
@@ -15,20 +18,37 @@ public class Grid {
      * @param number Número a validar
      * @return true si el número puede ser colocado, false en caso contrario
      */ 
-    boolean validation (int row, int column, int number) {
-        /** Validación de filas */
-        for (int i = 0; i < size; i++) {
-            if (grid[i][column] == number) {
-                return false;
-            }
+    /*boolean validation (int row, int column, int number) {
+        /** Validación de filas *
+        if (gridRowList.get(row) == number) {
+            return false;
         }
-        /** Validación de columnas */
-        for (int j = 0; j < size; j++) {
-            if (grid[row][j] == number) {
-                return false;
-            }
+        /** Validación de columnas *
+        if (gridColumnList.get(column) == number) {
+            return false;
         }
+        
         return true;
-    }
+    };*/
 
+    int randomNum() {
+        int num = (int) (Math.random() * (size + 1));
+        return num;
+    };
+
+    public int generateGrid() {
+    for (int i = 0; i < size; i++) {
+        for (int j = 0; j < size; j++) {
+            int randomNumber;
+            gridColumnList = new ArrayList<>();
+            //do {
+                randomNumber = randomNum();
+                System.out.println(randomNumber + " ");
+            //} while (/*validation(i, j, randomNumber) == true*/ false);
+            gridColumnList.add(randomNumber);
+        };
+    };
+
+    return 0;
+    };
 }
