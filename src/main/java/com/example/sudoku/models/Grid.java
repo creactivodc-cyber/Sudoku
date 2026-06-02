468817a5822efda6000b0699eb2ab198bef24fa5
robert-funes <creactivodc@gmail.com>
2026-06-01 20:08:28 -0500

    refactor: mejorar la legibilidad del código en la clase Grid

--- src/main/java/com/example/sudoku/models/Grid.java
+++ src/main/java/com/example/sudoku/models/Grid.java
@@ -1,21 +1,29 @@
 package com.example.sudoku.models;
 
-
-// Clase para generar la cuadricula de números aleatoriamente
+/** Clase para generar la cuadrícula de números aleatoriamente */
 public class Grid {
-
+    
+    /** Tamaño de la cuadrícula */
     int size = 6;
+    /** Cuadrícula de números */
     int grid[][] = new int[size][size];
-
+    
+    /**
+     * Valida si un número puede ser colocado en una posición específica de la cuadrícula
+     * @param row Fila donde se desea colocar el número
+     * @param column Columna donde se desea colocar el número
+     * @param number Número a validar
+     * @return true si el número puede ser colocado, false en caso contrario
+     */ 
     boolean validation (int row, int column, int number) {
-        // Recorrido de filas
-        for (int i = 0; i < 6; i++) {
+        /** Validación de filas */
+        for (int i = 0; i < size; i++) {
             if (grid[i][column] == number) {
                 return false;
             }
         }
-        // Recorrido de columnas
-        for (int j = 0; j < 6; j++) {
+        /** Validación de columnas */
+        for (int j = 0; j < size; j++) {
             if (grid[row][j] == number) {
                 return false;
             }
