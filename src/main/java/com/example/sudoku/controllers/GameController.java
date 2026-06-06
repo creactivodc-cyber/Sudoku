2012697143d84fc8967d2560f4e1467923b57d21
Salomon Jimenez <salomon.jimenez@correounivalle.edu.co>
2026-06-05 23:20:39 -0500

    Modificaciones UI, documentacion del controlador y clase CharField

--- src/main/java/com/example/sudoku/controllers/GameController.java
+++ src/main/java/com/example/sudoku/controllers/GameController.java
@@ -45,36 +45,60 @@ public class GameController {
         }
     }
 
+    /**
+     * Inicializa la interfaz grafíca del tablero de Sudoku.
+     * Recorre y posiciona 36 casillas (6x6) basadas en los valores del modelo.
+     * Aplica un enmascaramiento al 60% de las casillas.
+     * Calcular y dibujar dinámicamente los bordes con CSS.
+     */
     public void initialize() {
+        //Limpiar el gridLabel
         gridLabel.getChildren().clear();
 
         for (int fila = 0; fila < 6; fila++) {
             for (int col = 0; col < 6; col++) {
 
+                /*index Sirve para almacenar la formúla matematica, convierte las coordenadas (x, y)
+                      en una posición lineal. */
                 int index = (fila * 6) + col;
                 int number_result = gridValues.get(index);
 
-                //Probabilidad para ocultar casilla (60%)
+                // Probabilidad para ocultar casilla (60%)
                 boolean hideBox = Math.random() < 0.6;
 
                 CharField charField;
 
+                // Definir color de fondo
+                /* If - else en una sola línea
+                * Si hideBox es True, el fondo séra blanco, Si es False, séra gris.
+                * */
+                String backGroundColor = hideBox ? "-fx-background-color: #ffffff;" : "-fx-background-color: #e0e0e0;";
+
+                /*
+                * Si la casilla debe estar oculta, llama al constructor vacío (séra blanca la casilla)
+                * Si no, convierte el número de la lista a una letra (int -> char), llama a otro constructor
+                * para que se genere un número y no sea editable.
+                * */
                 if (hideBox) {
                     charField = new CharField();
-                    charField.setStyle("-fx-background-color: #ffffff;");
                 } else {
                     char numberChar = String.valueOf(number_result).charAt(0);
                     charField = new CharField(numberChar);
                     charField.setEditable(false);
-                    charField.setStyle("-fx-background-color: #e0e0e0;");
                 }
+
+                int bordeArriba = (fila == 0) ? 3 : 1;
+                int bordeDerecha = (col == 2 || col == 5) ? 3 : 1;
+                int bordeAbajo = (fila == 1 || fila == 3 || fila == 5) ? 3 : 1;
+                int bordeIzquierda = (col == 0) ? 3 : 1;
+
+                String estiloBordes = String.format("-fx-border-color: black; -fx-border-width: %d %d %d %d;",
+                        bordeArriba, bordeDerecha, bordeAbajo, bordeIzquierda);
+
+                charField.setStyle(backGroundColor + " " + estiloBordes);
 
                 charField.setPrefWidth(100);
                 gridLabel.add(charField, col, fila);
-
-                //charField.setAlignment(Pos.CENTER);
-                //System.out.println(gridValues.get(col).toString().charAt(0));
-                //CharField charField = new CharField(gridValues.get(col).toString().charAt(0));
 
             }
         }
--- src/main/java/com/example/sudoku/models/CharField.java
+++ src/main/java/com/example/sudoku/models/CharField.java
@@ -4,21 +4,37 @@ import javafx.geometry.Pos;
 import javafx.scene.control.TextField;
 import javafx.scene.text.Font;
 
+/** Clase para la generación de caracteres en la grilla del Sudoku */
 public class CharField extends TextField {
 
+    /** Atributo para almacenar el número y luego convertirlo a Char */
     private char num_char;
 
+    /** Constructor para las casillas fijas.
+     * Inicializa la casilla mostrando la pista generada automáticamente
+     * por la máquina.
+     * @param letter El carácter numérico que se asignara y mostrara en la casilla.
+     */
     public CharField(char letter) {
         this.num_char = letter;
         super.setText(String.valueOf(letter));
         stylesApply();
     }
 
+    /**
+     * Constructor para las casillas vacías.
+     * Inicializa la casilla sin texto, lista para que el jugador digite
+     * su número.
+     */
     public CharField() {
         super.setText("");
         stylesApply();
     }
 
+    /**
+     * Aplica los estilos visuales predeterminados a la casilla.
+     * Define la fuente, centra el texto y establece un tamaño fijo de 100x100.
+     */
     private void stylesApply(){
         this.setFont(Font.font("Arial Bold", 20));
         this.setAlignment(Pos.CENTER);
