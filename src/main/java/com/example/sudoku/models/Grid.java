18c1301f393482b63ba48164071c6898e20ece34
robert-funes <creactivodc@gmail.com>
2026-06-06 22:47:12 -0500

    Refactorizar la clase Grid: eliminar cuadrículas predefinidas, mejorar la validación y agregar algoritmo de backtracking para generar la cuadrícula.

--- src/main/java/com/example/sudoku/models/Grid.java
+++ src/main/java/com/example/sudoku/models/Grid.java
@@ -6,17 +6,6 @@ public class Grid {
     
     /** Tamaño de la cuadrícula */
     int size = 6;
-    int grid[][] = new int[size][size];
-
-    /** Cuadrícula predefinida para pruebas */
-    int grid2[][] = {
-        {5, 1, 4, 3, 6, 2},
-        {3, 6, 2, 1, 4, 5},
-        {6, 2, 1, 5, 3, 4},
-        {4, 3, 5, 2, 1, 6},
-        {2, 4, 6, 3, 5, 1},
-        {1, 5, 3, 4, 2, 6}
-    };
 
     /** Listas dinámicas para almacenar los números de las filas y columnas */
     ArrayList<Integer> gridList = new ArrayList<>();
@@ -40,28 +29,51 @@ public class Grid {
      * @param number Número a validar
      * @return true si el número puede ser colocado, false en caso contrario
     */ 
-    boolean validation (int row, int column, int number, int[][] grid) {
-        /** Validación de filas */
-        for (int i = 0; i < size; i++) {
-             if (grid[row][i] == number) {
-                return true;
-            }
-        }
-    
-        /** Validación de columnas */
-        for (int i = 0; i < size; i++) {
-            if (grid[i][column] == number) {
-                return true;
-            }
-        }
-        return false;
-    };
-
-   
     int randomNum() {
         int num = (int) (Math.random() * size + 1);
         return num;
     };
+
+
+    boolean validation (int row, int column, int number) {
+        
+        // Validar en la fila
+        for (int i = 0; i < size; i++) {
+            int index = row * size + i;
+            if (i == column) continue;
+            if (gridList.get(index) == number) {
+                System.out.println("Validando número " + number + " en la posición (" + index + ") si es igual a " + gridList.get(index) + " en la fila " + row + " columna " + column);
+                return true;
+            }
+        }
+
+        // Validar en la columna
+        for (int i = 0; i < size; i++) {
+            int index = i * size + column;
+            if (i == row) continue;  
+            if (gridList.get(index) == number) {
+                return true;
+            }
+        }
+
+        // Validar en el bloque 2x3
+        int startRow = (row / 2) * 2;
+        int startCol = (column / 3) * 3;
+
+        for (int i = startRow; i < startRow + 2; i++) {
+            for (int j = startCol; j < startCol + 3; j++) {
+                int index = i * size + j;
+                if (i == row && j == column) continue;
+                if (gridList.get(index) == number) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+        
+    };
+
 
     public void generateGrid() {
         for (int i = 0; i < size; i++) {
@@ -80,26 +92,63 @@ public class Grid {
     };
 
     public void generateGrid2() {
+        gridList.clear();
+        /** Inicializar la cuadrícula con ceros */
+        for (int i = 0; i < size * size; i++) {
+            gridList.add(0);
+        }
 
-        for (int i = 0; i < 2; i++) {
-            for (int j = 0; j < size; j++) {
-                int randomNumber;
-                randomNumber = randomNum();
-                
-                if (!validation(i, j, randomNumber, grid) && randomNumber != 0) {
-                    grid[i][j] = randomNumber;
+        /** Algoritmo de backtracking para llenar la cuadrícula */
+        int fila = 0;
+        int columna = 0;
+        int[] ultimoIntento = new int[size * size];
+        while (fila < size) {
+            int indice = fila * size + columna;
+            boolean encontrado = false;
+            for (int num = ultimoIntento[indice] + 1; num <= size; num++) {
+                gridList.set(indice, num);
+                if (fila == 0 && columna == 0) {
+                    encontrado = true;
+                    ultimoIntento[indice] = num;
+                    System.out.println("(" + fila + "," + columna + ") = " + num + " OK");
+                    break;
+                } else if (!validation(fila, columna, num)) {
+                    encontrado = true;
+                    ultimoIntento[indice] = num;
+                    System.out.println("(" + fila + "," + columna + ") = " + num + " OK");
+                    break;
+                } else {
+                    gridList.set(indice, 0);
                 }
-                
-            };     
+            }
+            if (encontrado) {
+                // avanzo a la siguiente celda
+                columna++;
+                if (columna == size) {
+                    columna = 0;
+                    fila++;
+                }
+            } else {
+                // retrocedo (backtrack)
+                ultimoIntento[indice] = 0;
+                gridList.set(indice, 0);
+                columna--;
+                if (columna < 0) {
+                    columna = size - 1;
+                    fila--;
+                }
+                // si fila se vuelve negativa, no hay solucion
+                if (fila < 0) {
+                    System.out.println("No hay solucion posible.");
+                    return;
+                }
+            }
+        }
+    }
             
-
-            //gridList.addAll(gridColumnList.subList(0, size));
-            System.out.println("Fila " + (i + 1) + ": " + grid[i][0] + " " + grid[i][1] + " " + grid[i][2] + " " + grid[i][3] + " " + grid[i][4] + " " + grid[i][5]);
-        };
-    };
- 
     public ArrayList<Integer> getGrid() {
-        //generateGrid2();
-        return gridList2;
+        generateGrid2();
+        System.out.println("Cuadrícula generada: " + gridList);
+        return gridList;
     }
 }
